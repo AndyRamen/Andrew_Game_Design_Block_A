059f4d5d771a997a63949ea2c322f39a1834fdaa
AndyRamen <51421461+AndyRamen@users.noreply.github.com>
2021-09-10 17:23:17 -0500

    Add files via upload

---
+++ loopingpt2.py
@@ -0,0 +1,25 @@
+import os
+
+os.system('cls')
+
+stars = int(input("Enter number of stars: "))
+stars2 = stars
+line = stars
+space = 0
+
+for i in range(line):
+    for counter in range(stars):
+        print("* ", end = " ")
+    stars-=1
+    
+    for k in range(space):
+        print("  ", end = " ")
+    space +=2
+
+    for counter in range(stars2):
+        print("* ", end = " ")
+
+    print()
+    stars2-=1
+
+print("Grundo")
