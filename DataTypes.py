139cb410b6164ad0fcc1a94c7e7c5eb6d66b20df
AndyRamen <namelessgaming234@gmail.com>
2021-09-13 09:42:46 -0500

    (commit on 'main')

---
+++ DataTypes.py
@@ -0,0 +1,20 @@
+#Andrew Cai
+#09/13/21
+#This is about data types
+#How  strings work
+#Integers: long (8 bytes), float (4 bytes), double (8 bytes), boolean
+#Float uses decimal. If you put 5.0 instead of 5, it will think it's a float instead of a long.
+#Boolean is true or false (binary).
+
+import os
+
+os.system('cls')
+userInput=input("type something ")
+print (type(userInput))
+try:
+    int(userInput)
+    check=True
+except ValueError:
+    check=False
+
+print(check)
