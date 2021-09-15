03f91890edda8e48e324135c5f3f523d1eaee703
AndyRamen <namelessgaming234@gmail.com>
2021-09-15 08:49:26 -0500

    (commit on 'main')

--- DataTypesChallenge.py
+++ DataTypesChallenge.py
@@ -18,18 +18,20 @@ var1=int(input("Type your first number: "))
 var2=int(input("Type your second number: "))
 print("Note: In subtraction the second number will be subtracted from the first. In division, it will be the first number divided by the second.")
 
+print()
 addanswer = var1+var2
 print("Addition answer:", addanswer)
-
+print()
 subtanswer = var1-var2
 print("Subtraction answer:", subtanswer)
-
+print()
 multanswer = var1*var2
 print("Multiplication answer:", multanswer)
-
+print()
 divanswer = var1/var2
 print("Division answer:", divanswer)
-
+print()
 modanswer = var1%var2
 print("Modulus answer:", modanswer)
 print("The modulus tells you the remainder between the two numbers.")
+print()
