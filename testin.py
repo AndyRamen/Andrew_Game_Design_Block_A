ec7ddfa14d8d8305debb4d9eb54315d0979c93ad
AndyRamen <namelessgaming234@gmail.com>
2021-09-27 09:35:54 -0500

    (commit on 'main')

--- testin.py
+++ testin.py
@@ -2,11 +2,12 @@ import os
 os.system('cls')
 
 import random
-print("I will choose one word and give you multiple hints. Try guessing. Type anything to continue.")
+print("I will choose one word out of 5. Try guessing. Type 'yes' to continue")
 answer = input("Do you want to play? ")
 userInput = answer
 print("Guess the word! Guess either dog, fish, cat, snake, or rabbit.")
 while ('y' in answer):
+    tries=5
     start=1
     words= ["dog", "fish", "cat", "snake", "rabbit"]
     myword = random.choice(words)
@@ -17,7 +18,7 @@ while ('y' in answer):
     
     print("New word!")
     print()
-    while (start==1):
+    while (start>0):
         userInput= input("What is your guess? ")
         userInput=userInput.lower()
         if userInput== myword:
@@ -28,3 +29,8 @@ while ('y' in answer):
             userInput != myword
             print("Incorrect. Try again.")
             print()
+            tries-=1
+            print(tries)
+    if tries==0:
+        print("You're out of tries!")
+        break
