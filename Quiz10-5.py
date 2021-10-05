579bd1495da4f73d829843894bf71381a70580d2
AndyRamen <namelessgaming234@gmail.com>
2021-10-05 09:10:44 -0500

    (commit on 'main')

---
+++ Quiz10-5.py
@@ -0,0 +1,58 @@
+#This is for question 1:
+print(len('Hello World!'))
+print("End of Q1")
+print()
+#This is for question 2:
+strString = 'Study.com is a wonderful resource.'
+print(strString.find('great'))
+print(strString.find('wonderful'))
+print(strString.find('.'))
+print("End of Q2")
+print()
+#This is for question 3:
+strString = 'Study.com is a wonderful resource.'
+print(strString[15:24])
+print("End of Q3")
+print()
+
+#THis is for Question 4:
+print(strString[0])
+print("End of Q4")
+print()
+
+#This is for question 5:
+strString = 'Study.com is a wonderful resource'
+print(strString[15:24].find('der')+len(strString))
+print("End of Q5")
+print()
+
+#This is for question 6:
+var1 = 17
+var2 = 4
+result = var1%var2
+print(result)
+print("End of Q6")
+print()
+
+#This is for question 7:
+print("sdafdasfsfhrt")
+"\t" 
+print("dlgkjvslgkj")
+"\t"
+print("End of Q7")
+print()
+#This is for question 8:
+"\n"
+print("dsfjsaf")
+print("End of Q8")
+print()
+#THis is for question 9:
+var1 = "Hello"
+var2 = " World!"
+result2 = var1+var2
+print(result2)
+
+#This is for question 10:
+List = ['0', '1','2','3','4','6','7','8','9','10']
+List.insert(5, '5')
+print(List)
--- SecondLetterGuessingGame.py
+++ SecondLetterGuessingGame.py
@@ -5,42 +5,113 @@
 # Swhor the word to the user with the characters in spaces.
 # PLay as long as the user has turns or hasn't guessed the word.
 
+
 import random, os
 os.system('cls')
 
-compParts=["keyboard", "Monitors", "computer", "mouse", "case", "operating system"]
-print("Word Game ")
-print(" Guess what computer part I am thinking of. ")
-name= input("What is your name? ")
-counter=0
-answer = input( name + ", do you want to play? ")
-while ('y' in answer):
-    print(name + ", good luck! You have 5 chances to guess my word.")
-    turns=5
-    counter +=1
-    word = random.choice(compParts)
-    word = word.lower()
-    print(word) #Just for checking our code, remove later.
-    guesses = ' ' 
+def updateWord(word, guesses): #Function to update dashes
     for letter in word:
         if letter in guesses:
             print(letter, end=" ")
         else:
-            print('_', end=' ')
-    while turns > 0:
-        print()
-        newguess=input("Give me a letter: ")
-        newguess=newguess.lower()
-        if newguess in word:
-            guesses += newguess
-            print("You guessed one letter. ")
-        else:
-            turns-=1
-            print("Sorry, you have ", turns, " turns left.")
-        
-        for letter in word:
-            if letter in guesses:
-                print(letter, end=" ")
+            print('_', end= " ")
+#define function for menu
+def Menu():
+    print()
+    #Add instrpuctions here
+    print("          ----Menu----         ")
+    print(                                 )
+    print("           1. Animals          ")
+    print("           2. Computer parts         ")
+    print("           3. Fruits         ")
+    print("           4. Exit          ")
+    print("To play the game, select 1-3 and to exit select 4.")
+    print()
+    sel=input("What would you like to do? ")
+    #Put in try and except here
+    sel=int(sel)
+    return sel
+
+def selWord():
+    if sel == 1:
+        word= random.choice(animals)
+    elif sel == 2:
+        word= random.choice(compParts)
+    else:
+        word= random.choice(fruits)
+
+animals=["dog", "tiger", "elephant", "lion"]
+fruits=["apple", "strawberry", "blueberry", "pear"]
+compParts=["keyboard", "monitor", "case", "mouse", "trackpad"]
+name= input("What is your name? ")
+counter=0
+sel = Menu()
+while sel !=4:
+    if sel == 2:
+        print(name + ", good luck! You have 5 chances to guess my word.")
+        turns=5
+        counter +=1
+        word = random.choice(compParts)
+        word = word.lower()
+        print(word) #Just for checking our code, remove later.
+        guesses = ' ' 
+        updateWord(word, guesses)
+
+        while turns > 0:
+            print()
+            newguess=input("Give me a letter: ")
+            newguess=newguess.lower()
+            if newguess in word:
+                guesses += newguess
+                print("You guessed one letter. ")
             else:
-                print('_', end= " ")
-    answer=input(name + " Do you want to play again?")
+                turns-=1
+                print("Sorry, you have ", turns, " turns left.")
+    
+    if sel == 1:
+        print(name + ", good luck! You have 5 chances to guess my word.")
+        turns=5
+        counter +=1
+        word = random.choice(animals)
+        word = word.lower()
+        print(word)
+        guesses = ' ' 
+        updateWord(word, guesses)
+
+        while turns > 0:
+            print()
+            newguess=input("Give me a letter: ")
+            newguess=newguess.lower()
+            if newguess in word:
+                guesses += newguess
+                print("You guessed one letter. ")
+            else:
+                turns-=1
+                print("Sorry, you have ", turns, " turns left.")
+
+    if sel == 3:
+        print(name + ", good luck! You have 5 chances to guess my word.")
+        turns=5
+        counter +=1
+        word = random.choice(fruits)
+        word = word.lower()
+        print(word) 
+        guesses = ' ' 
+        updateWord(word, guesses)
+
+        while turns > 0:
+            print()
+            newguess=input("Give me a letter: ")
+            newguess=newguess.lower()
+            if newguess in word:
+                guesses += newguess
+                print("You guessed one letter. ")
+            else:
+                turns-=1
+                print("Sorry, you have ", turns, " turns left.")
+        
+        
+        
+        updateWord(word,guesses)
+    os.system('cls')
+    sel=Menu()
