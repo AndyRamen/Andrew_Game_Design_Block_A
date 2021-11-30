c12e226eabc72ddb5245570b6fad381128937cbd
AndyRamen <namelessgaming234@gmail.com>
2021-11-30 09:27:37 -0600

    update

--- FinalProjectTest.py
+++ FinalProjectTest.py
@@ -9,5 +9,13 @@ while run:
     bg1 = pygame.image.load('GameImages\purple space.jpg')
     screen.blit(bg1, (0,0))
     #screen.fill(myColor)
+    ship1 = pygame.image.load('GameImages\enemy ship 1Resized.png')
+    screen.blit(ship1, (20,20))
+    ship2 = pygame.image.load('GameImages\enemy ship 1Resized.png')
+    screen.blit(ship2,  (70,20))
+    ship3 = pygame.image.load('GameImages\enemy ship 4Resized.png')
+    screen.blit(ship3, (120,20))
     pygame.display.set_caption("Space Invaders")
     pygame.display.flip()
+
+pygame.display.quit()
